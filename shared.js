63dce9b6fbef315c5dc3ba7953cbd0d5dc5c5529
erobert <etiennerobert33@gmail.com>
2020-08-29 20:41:20 +0200

    improve dates formatting

--- shared.js
+++ shared.js
@@ -1,10 +1,10 @@
 function formatDate(date) {
-  const daysOfWeek = ["So", "Mo", "Die", "Mi", "Do", "Fr", "Sa"];
-  const dayOfWeek = daysOfWeek[date.getDay()];
-  const dayOfMonth = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  const hour = date.getHours() + 1;
-  const minutes = date.getMinutes() + 1;
-  return `${dayOfWeek}. ${dayOfMonth}.${month}.${year} um ${hour}.${minutes}`;
+  const daysOfWeek = ["So.", "Mo.", "Die.", "Mi.", "Do.", "Fr.", "Sa."];
+  const dayOfWeek = daysOfWeek[date.getDay()].padEnd(4);
+  const dayOfMonth = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear().toString();
+  const hour = (date.getHours() + 1).toString().padStart(2);
+  const minutes = (date.getMinutes() + 1).toString().padStart(2, "0");
+  return `${dayOfWeek} ${dayOfMonth}.${month}.${year} - ${hour}.${minutes}`;
 }
