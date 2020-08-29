a46adb2e07fa70a582aaa320d5966c927c410cb3
erobert <etiennerobert33@gmail.com>
2020-08-29 21:09:26 +0200

    backend saves in file

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
 app.use(cors()); // TODO restrict access
 
-const dates = [];
+let dates = [];
 let nextId = 0;
+
+const filename = "dates.json";
+
+fs.readFile(filename, "utf8", (err, data) => {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  dates = JSON.parse(data);
+});
+
+function updateFile() {
+  fs.writeFile(filename, JSON.stringify(dates), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+}
 
 app.use(
   bodyParser.urlencoded({
@@ -20,6 +40,7 @@ app.use(bodyParser.json());
 app.post("/dates", (req, res) => {
   const date = { id: nextId++, ...req.body };
   dates.push(date);
+  updateFile();
   res.send(date);
 });
 
@@ -30,6 +51,7 @@ app.get("/dates", (req, res) => {
 app.delete("/dates/:id", (req, res) => {
   const index = dates.findIndex((date) => date.id == req.params.id);
   if (index != -1) dates.splice(index, 1);
+  updateFile();
   res.send("OK");
 });
 
