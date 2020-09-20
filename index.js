66e8efbb602d2efe559844f02c72dd9f1955c2d1
erobert <etiennerobert33@gmail.com>
2020-09-20 07:56:43 +0200

    create smooth image loading

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 'use strict';
 
-const main = document.getElementsByTagName('main')[0];
-const scrollIcon = document.getElementById('scrollIcon');
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+  :host {
+    display: inline-block;
+  }
+  img {
+    width: 100%;
+    height: auto;
+    display: block;
+    opacity: 0;
+  }
+</style>`;
 
-scrollIcon.addEventListener('click', () =>
-  main.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' })
-);
+class PlaceholderImage extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    const src = this.getAttribute('src');
+    const width = this.getAttribute('width');
+    const height = this.getAttribute('height');
+    const color = this.getAttribute('color');
+
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    const image = new Image();
+    image.width = parseInt(width);
+    image.height = parseInt(height);
+    image.onload = () => {
+      image.animate([{ opacity: 0 }, { opacity: 1 }], {
+        duration: 1000,
+        fill: 'forwards',
+      });
+    };
+    this.shadowRoot.appendChild(image);
+
+    this.style.background = color;
+    image.src = src;
+  }
+}
+customElements.define('placeholder-image', PlaceholderImage);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const main = document.getElementsByTagName('main')[0];
+  const scrollIcon = document.getElementById('scrollIcon');
+
+  scrollIcon.addEventListener('click', () =>
+    main.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' })
+  );
+});
