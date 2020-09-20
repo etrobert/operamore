1fc099d6991d614141e5c4409acd5b5eb81eb650
erobert <etiennerobert33@gmail.com>
2020-09-20 05:05:51 +0200

    use single quotes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
-const main = document.getElementsByTagName("main")[0];
-const scrollIcon = document.getElementById("scrollIcon");
+const main = document.getElementsByTagName('main')[0];
+const scrollIcon = document.getElementById('scrollIcon');
 
-scrollIcon.addEventListener("click", () =>
-  main.scrollIntoView({ behavior: "smooth", block: "start", inline: "start" })
+scrollIcon.addEventListener('click', () =>
+  main.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' })
 );
